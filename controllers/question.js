f05ffd83f32a9a4dbcfdac1710d1531058236be8
Cathy <Cathy347Le@gmail.com>
2019-02-20 17:34:26 -0500

    updated controllers and views for adding a new question

--- controllers/question.js
+++ controllers/question.js
@@ -3,7 +3,13 @@ module.exports = {
     res.render("question/new");
   },
   create: function(req, res) {
-    res.redirect("/");
+    const { title, description } = req.body;
+    Question.create({
+      title,
+      description
+    }).then(question => {
+      res.redirect(`/question/${question.id}`);
+    });
   },
   show: function(req, res) {
     res.render("question/show");
