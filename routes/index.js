8f4e7f9146891c06b5c3428cb182ce00af3189a4
Cathy <Cathy347Le@gmail.com>
2019-02-20 17:32:44 -0500

    display seed data to homepage

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const Question = require("../models/index");
 
 router.get("/", function(req, res) {
-  res.render("index");
+  Question.find({}).then(questions => {
+    res.render("index", { questions });
+  });
 });
 
 router.use("/question", require("./question.js"));
