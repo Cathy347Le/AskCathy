5d652e04ca64eb649c321b15de73ef9b162509e6
Cathy <Cathy347Le@gmail.com>
2019-02-22 16:54:47 -0500

    update code in connection.js for deployment

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 if (process.env.NODE_ENV == "production") {
   mongoose.connect(process.env.DB_URL);
 } else {
-  mongoose.connect("mongodb://localhost/AskCathy");
+  mongoose.connect("mongodb://localhost/queueunderflow", {
+    useNewUrlParser: true
+  });
 }
 
 mongoose.Promise = Promise;
