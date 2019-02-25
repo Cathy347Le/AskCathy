406b5bc3baac0c5f10eccc0a6bbeeca0653c541b
Cathy <Cathy347Le@gmail.com>
2019-02-24 23:27:27 -0500

    add views for user

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Question = require("../models/index");
+const { Question } = require("../models/index");
 
 router.get("/", function(req, res) {
   Question.find({}).then(questions => {
